49c3c8d83af97f7c356501e7abda49e5004bacec
Christie Hill <c@chilldsgn.com>
2022-01-18 21:35:05 +0200

    It's not perfect, but it works.

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-//https://auth0.com/blog/node-js-and-express-tutorial-building-and-securing-restful-apis/
+import 'dotenv/config';
+import express from 'express';
+import fetch from 'node-fetch';
+import rateLimit from 'express-rate-limit';
+const app = express();
+const port = 3000;
 
-console.log('hi');
+const limiter = rateLimit({
+    windowMs: 1000, // 1s
+    max: 1 // limit each IP address to 1 request per windowMs
+});
+
+app.use(limiter); // apply to all requests
+app.get('/', (request, response) => response.send('hullabaloo'));
+
+app.get('/api/weather/:city', async(request, response) => {
+    const city = request.params.city;
+    try {
+        const data = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.OWM_API_KEY}`)
+        .then(data => data.json());
+        response.send(data);
+    } catch(error) {
+        return response.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+});
+
+app.listen(port, () => console.log('App running at port 3000'));
